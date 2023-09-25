fb6ace85fa572dbfc274deb0bf53a19a633a7a1b
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-09-25 14:26:11 +0530

    Add Routing part

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -13,11 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/login',function(){
-return view('login');
+Route::get('/home',function(){
+return view('home');
 });
+
+
+Route::get('/customer',function(){
+    return view('customer-Page');
+    });
+
+
+    Route::get('/item',function(){
+        return view('item-Page');
+        });
